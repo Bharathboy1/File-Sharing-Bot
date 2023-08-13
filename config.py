35df2042e97310f886b67a0248e6c37f2845745c
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-08-13 11:22:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "{filename}\n\n🎬ᴊᴏɪɴ ɴᴏᴡ: [Filmztube  ⎚](https://t.me/filmztube)")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "{filename}\n\n🎬ᴊᴏɪɴ ɴᴏᴡ: <ᴀ ʜʀᴇғ='ʜᴛᴛᴘs://ᴛ.ᴍᴇ/ғɪʟᴍᴢᴛᴜʙᴇ'>ғɪʟᴍᴢᴛᴜʙᴇ</ᴀ>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!\n\n🎬ᴊᴏɪɴ ɴᴏᴡ: [Filmztube  ⎚](https://t.me/filmztube)"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!\n\n🎬ᴊᴏɪɴ ɴᴏᴡ: <a href='https://t.me/filmztube'>ғɪʟᴍᴢᴛᴜʙᴇ</a>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1447209433)
